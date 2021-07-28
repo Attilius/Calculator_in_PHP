8dbafbc8d88f4576a0737b214f3f56fafa4fafbd
Attilius <leo20@freemail.hu>
2021-07-28 12:48:45 +0200

    Add App folder and index.php

---
+++ App/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculator1.0</title>
+</head>
+<body>
+    <div class="calculator">
+        <form action="" method="POST">
+            <div class="display"> 
+            </div>
+        <div class="control">
+            <input type="text" name="number1" id="number1">
+            <select name="operators" id="operators">
+                <option value="plus">+</option>
+                <option value="minus">-</option>
+                <option value="multiply">*</option>
+                <option value="divide">/</option>
+            </select>
+            <input type="text" name="number2" id="number2">
+        </div>
+        <button type="submit" name="submit">Calc</button>
+        </form>
+    </div>
+</body>
+</html>
